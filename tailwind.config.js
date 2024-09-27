65f615ea1ed1d8249b41d908df845740040fbaad
laiba abbasi <laibaabbasi2022@gmail.com>
2024-09-28 00:55:22 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        customPurple: '#2f012f',  // This is your custom color
+      },
+    },
+  },
+  plugins: [],
+};
